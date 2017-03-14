b4db8973609a8d89172f4460921adf1c7559c490
Chris <jc.honkanen@gmail.com>
2017-03-14 18:56:40 +0000

    Mesh Data Components

---
+++ QSO/Mesh.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Component.h"
+#include <GL\glew.h>
+struct MeshData {
+	GLuint VAO;
+	GLuint VBO;
+	GLuint EBO;
+};
+class Mesh :
+	public Component
+{
+public:
+	Mesh(std::string name);
+	virtual ~Mesh();
+	MeshData mesh;
+};
+
